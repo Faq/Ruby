445ea1bbf8c3ff29c11035b73c884edf2ef35c76
FAQ <ainarsh@gmail.com>
2021-11-02 16:03:55 +0200

    Seed script using URI

--- opendata/db/seeds.rb
+++ opendata/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+url = 'https://data.gov.lv/dati/dataset/5d0c9a64-b7b2-494e-b77d-22d48225791b/resource/8ea0ee31-1bea-4336-bbe4-2e66ccdadd1b/download/covid_19_valstu_saslimstibas_raditaji.csv'
+CSV.foreach(Rails.root.join(URI.parse(url).open), row_sep: :auto, headers: :first_row, col_sep: ';') do |row|
+  Stream.create({
+                  date: row[0],
+                  country: row["Valsts"],
+                  twoweek_cumulative: row["14DienuKumulativaIncidence"],
+                  departure: row["Izcelosana"],
+                  self_isolation: row["Pasizolacija"]
+                })
+end
