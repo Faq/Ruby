e0cdc18b099130245cf24d59ca5ea13c0c942fec
FAQ <ainarsh@gmail.com>
2021-11-03 06:54:22 +0200

    gem has_scope and basic search implementation

--- opendata/app/controllers/streams_controller.rb
+++ opendata/app/controllers/streams_controller.rb
@@ -1,9 +1,13 @@
 class StreamsController < ApplicationController
-  before_action :set_stream, only: %i[ show edit update destroy ]
+  before_action :set_stream, only: %i[show edit update destroy]
 
+  has_scope :country
+  has_scope :by_date, type: :hash, using: [:from_date, :to_date], as: :date
+  has_scope :page, default: 1
+  
   # GET /streams or /streams.json
   def index
-    @streams = Stream.all
+    @streams = apply_scopes(Stream.all)
   end
 
   # GET /streams/1 or /streams/1.json
--- opendata/app/models/stream.rb
+++ opendata/app/models/stream.rb
@@ -1,2 +1,12 @@
 class Stream < ApplicationRecord
+  scope :country, ->(query) { where('country ilike ?', "%#{query}%") }
+
+  def self.by_date(from_date, to_date)
+    result = all
+
+    result = result.where('date >= ?', from_date) if from_date.present?
+    result = result.where('date <= ?', to_date) if to_date.present?
+
+    result
+  end
 end
