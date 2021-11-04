1dbe2140937ef0f54f1c629c91c9ee0c880b68b7
FAQ <ainarsh@gmail.com>
2021-11-04 19:07:22 +0200

    Add area chart with all its config

--- opendata/app/controllers/charts_controller.rb
+++ opendata/app/controllers/charts_controller.rb
@@ -1,6 +1,11 @@
 class ChartsController < ApplicationController
-  def completed_tasks
-    data = [['Country', 'Cumulative']] + Stream.where(date: 1.weeks.ago.midnight..Time.now).group(:country).sum(:twoweek_cumulative).map { |country, count| [country, count] }
-    render json: data
+  def area_chart_tasks
+    area_data = Stream.group_by_day(:date).sum(:twoweek_cumulative)
+    render json: area_data
+  end
+
+  def geo_chart_tasks
+    geo_data = [['Country', 'Cumulative']] + Stream.where(date: 1.weeks.ago.midnight..Time.now).group(:country).sum(:twoweek_cumulative).map { |country, count| [country, count] }
+    render json: geo_data
   end
 end
--- opendata/config/routes.rb
+++ opendata/config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   resources :streams
 
   namespace :charts do
-    get "completed_tasks"
+    get "area_chart_tasks"
+    get "geo_chart_tasks"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
